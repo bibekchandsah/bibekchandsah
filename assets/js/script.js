1b6b63f5cb2a821d85eed0935bcc91a8a2a0f6c5
bibek10550 <bibeksha48@gmail.com>
2024-04-22 18:41:33 +0530

    added exe file and updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,39 +112,85 @@ document.addEventListener('keydown', function (event) {
 
 
 // swiper
-const swiper = new Swiper('.swiper', {
+// const swiper = new Swiper('.swiper', {
+//     loop: true,
+//     grabCursor: true,
+//     // spaceBetween: 48,
+//     // centeredSlides: true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
+//     // breakpoints: {
+//     //     640: {
+//     //         slidesPerView: 3,
+//     //     },
+//     // },
+//     effect: "coverflow",
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     coverflowEffect: {
+//         rotate: 50,
+//         stretch: 0,
+//         depth: 100,
+//         modifier: 1,
+//         slideShadows: true,
+//     },
+//     mousewheel: true,
+//     keyboard: true,
+// });
+
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
     loop: true,
-    grabCursor: true,
-    // spaceBetween: 48,
-    // centeredSlides: true,
+    centerSlide: 'true',
+    fade: 'true',
+    // mousewheel: true,
+    keyboard: true,
+    grabCursor: 'true',
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
     },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
         dynamicBullets: true,
     },
-    // breakpoints: {
-    //     640: {
-    //         slidesPerView: 3,
-    //     },
-    // },
-    effect: "coverflow",
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    mousewheel: true,
-    keyboard: true,
-});
+    // effect: "coverflow",
+    // coverflowEffect: {
+    //     rotate: 50,
+    //     stretch: 0,
+    //     depth: 100,
+    //     modifier: 1,
+    //     slideShadows: true,
+    // },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+})
+
